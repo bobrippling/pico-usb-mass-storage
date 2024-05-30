057188dceb33aa6f8a2ba7edcbf44971cd759313
Rob Pilling <robpilling@gmail.com>
2024-05-30 22:04:42 +0100

    `#[overlay]` InquiryCommand as a proof-of-concept

--- src/scsi/commands/command.rs
+++ src/scsi/commands/command.rs
@@ -39,7 +39,8 @@ impl Command {
             OpCode::Read12 => Ok(Command::Read(checked_extract::<Read12Command>(cbw)?.into())),
             OpCode::ReadCapacity10 => Ok(Command::ReadCapacity(checked_extract(cbw)?)),
             OpCode::ReadFormatCapacities => Ok(Command::ReadFormatCapacities(checked_extract(cbw)?)),
-            OpCode::Inquiry => Ok(Command::Inquiry(checked_extract(cbw)?)),
+            // TODO: return &Command and avoid the copy here
+            OpCode::Inquiry => Ok(Command::Inquiry(*overlay(cbw)?)),
             OpCode::TestUnitReady => Ok(Command::TestUnitReady(checked_extract(cbw)?)),
             OpCode::ModeSense6 => Ok(Command::ModeSense(checked_extract::<ModeSense6Command>(cbw)?.into())),
             OpCode::ModeSense10 => Ok(Command::ModeSense(checked_extract::<ModeSense10Command>(cbw)?.into())),
@@ -61,6 +62,12 @@ impl Command {
     }
 }
 
+fn overlay<'a, T: overlay::Overlay>(cbw: &'a CommandBlock) -> Result<&'a T, Error> {
+    T::overlay(&cbw.bytes)
+        .map_err(|e| match e {
+            overlay::Error::InsufficientLength => Error::InsufficientDataForCommand
+        })
+}
 
 fn checked_extract<T>(cbw: &CommandBlock) -> Result<T, Error>
 where
